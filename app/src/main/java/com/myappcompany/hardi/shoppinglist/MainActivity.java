1198e5a0ad1d3c332ffc5c920951cf72e2dc7266
Aleksandar Hardi <sasahardi@gmail.com>
2019-12-20 01:03:55 +0100

    First commit

---
+++ app/src/main/java/com/myappcompany/hardi/shoppinglist/MainActivity.java
@@ -0,0 +1,93 @@
+package com.myappcompany.hardi.shoppinglist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText pass;
+    private Button btnLogin;
+    private TextView signUp;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mDialog=new ProgressDialog(this);
+
+        email=(EditText) findViewById(R.id.email_login);
+        pass=(EditText) findViewById(R.id.password_login);
+        signUp=(TextView) findViewById(R.id.signup_text);
+
+        btnLogin=(Button) findViewById(R.id.btn_login);
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail=email.getText().toString().trim();
+                String mPassword=pass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Required..");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(mPassword)){
+                    pass.setError("Required..");
+                    return;
+                }
+
+                mDialog.setMessage("Processing..");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }else {
+                            Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+    }
+}
